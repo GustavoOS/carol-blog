2995a57e7b2fd52aade986ef811a19f2c8c4933c
Gustavo Souza <gustavosouza@Mac-mini-de-Gustavo.local>
2025-06-01 11:58:56 -0300

    home page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Carolina Souza';
+export const SITE_DESCRIPTION = 'Bemm-vindo ao Blog da Carol, onde compartilho minhas experiências e aprendizados sobre psicologia perinatal.';
