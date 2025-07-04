3ee8c6994918f38092622ace46032053d3528226
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2025-07-03 21:17:35 -0300

    change site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Carolina Souza';
-export const SITE_DESCRIPTION = 'Bem-vindo ao Blog da Carol, onde compartilho minhas experiências e aprendizados sobre psicologia perinatal.';
+export const SITE_DESCRIPTION = 'Acolhimento e psicoterapia para mulheres que sonham com a maternidade, mas carregam medos, lutos e ansiedade no caminho.';
